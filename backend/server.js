659a14ed279dbd9ff0b3c3af4a30a2559d60bdb8
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-11-11 02:14:12 -0800

    Developed server.js and created files to connect to DB and test the connection. Connection was successfully established and for now, all IPs have been whitellisted for access on my profile.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require('./config/db');
 
 require("dotenv").config();
 
 const app = express();
+
+//Establish DB connection
+connectDB();
 
 app.get('/', (req, res) => res.send('API is Running'));
 const port = process.env.PORT || 5000;
