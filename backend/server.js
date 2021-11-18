4d910f68cb891edeca91410a58e8d106a221e1d3
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2021-11-17 18:14:16 -0800

    Added error handlers

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const errorHandler = require("./middlewares/error-handler");
 const connectDB = require("./config/db");
 
 require("dotenv").config();
@@ -18,6 +19,10 @@ app.get("/", (req, res) => res.send("API is Running"));
 app.use("/api/auth", require("./routes/api/auth.js"));
 
 app.use("/api/users", require("./routes/api/users.js"));
+
+app.use(errorHandler.handleNotFound);
+app.use(errorHandler.handleError);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
