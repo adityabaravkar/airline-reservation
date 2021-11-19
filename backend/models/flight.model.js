ea60c88c7d290c9344719237cc9e23e134df9993
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-11-19 03:59:30 -0800

    Developed the flight model and route files. I have appropriately commented the fields where I was tsting stuff. for now, the API is not posting or reporting errors correctly for flight model. it gives out 404 instead of the 400. FIX ME: FIX THE FLIGHT MODEL ROUTING AND API

---
+++ backend/models/flight.model.js
@@ -0,0 +1,45 @@
+// NOTE: CONSULT MEETING NOTES 2 AND 3 FOR MORE DATA ON OBJ. ATTRIBS AND SCHEMA NOTES!
+const mongoose = require ('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    departureFrom: {
+        type: String,
+        required: true
+    },
+    
+    arrivalAt: {
+        type: String,
+        required: true
+    },
+    flightNo: {
+        type: String,
+        required: true
+    },
+    arrivalDate: {
+        type: Date,
+        // DISABLING FOR TESTING
+        // required: true
+    },
+    departureDate: {
+        type: Date,
+        // DISABLING FOR TESTING
+        // required: true
+    },
+    //Seat object is an array; I dunno how this works yet so putting placeholder code here for now...
+    seatStatus: {
+        // FIX ME: putting default array for now; needs to be an array of object type 'Seat' which has 'SeatStatus' (boolean?) objects in it
+        type: Array,
+        // FIX ME: confirm whether this is required? how do we pass a constructor of certain type in js?
+        // DISABLING FOR TESTING
+        // required: true
+    },
+    employedBy: {
+        // FIX ME: is supposed to be an 'airline' type object so... I dunno?
+        type: Airline,
+        // DISABLING FOR TESTING
+        // required: true
+    }
+
+});
+
+module.exports = flight = mongoose.model('flight', flightSchema);
--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,23 @@ const app = express();
 //Establish DB connection
 connectDB();
 
+// (From) Shahbaz: Initializing middleware; works using Express.js
+
+app.use(express.json({
+  extended: false
+}));
+
+
 app.get('/', (req, res) => res.send('API is Running'));
 
 // Defining Route
 app.use('/api/users', require('./routes/api/users.js'));
+// FIX ME: THE FOLLOWING ROUTE IS NOT WORKING! I HAVE DISABLED THE REQUIRED FIELDS AND YET IT IS STILL RESULTING IN 404s INSTEAD OF 400s!
+app.use('api/flight', require('./routes/api/flight.js'));
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
